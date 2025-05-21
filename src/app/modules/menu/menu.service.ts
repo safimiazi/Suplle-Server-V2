b0ebc0fb4463479eeb3272e4245be9291a19acc8
safimiazi <mohibullamiazi@gmail.com>
2025-05-21 17:05:32 +0600

    refactor menu routes and controller for improved structure and clarity

--- src/app/modules/menu/menu.service.ts
+++ src/app/modules/menu/menu.service.ts
@@ -1,114 +1,119 @@
+import status from "http-status";
+import AppError from "../../errors/AppError";
+import { MenuModel } from "./menu.model";
+import { validateData } from "../../middlewares/validateData ";
+import { IMenu } from "./menu.interface";
+import { menuPostValidation, menuUpdateValidation } from "./menu.validation";
+import { uploadImgToCloudinary } from "../../utils/sendImageToCloudinary";
+import { RestaurantModel } from "../restuarant/restuarant.model";
+import mongoose, { Types } from "mongoose";
 
-    import status from "http-status";
-    import AppError from "../../errors/AppError";
-    import { MenuModel } from "./menu.model";
-    import { validateData } from "../../middlewares/validateData ";
-    import { IMenu } from "./menu.interface";
-    import { menuPostValidation, menuUpdateValidation } from "./menu.validation";
-    import { uploadImgToCloudinary } from "../../utils/sendImageToCloudinary";
-    import { RestaurantModel } from "../restuarant/restuarant.model";
-    import mongoose, { Types } from "mongoose";
-    
+export const menuService = {
+  async postMenuIntoDB(
+    data: any,
+    file: Express.Multer.File & { path?: string }
+  ) {
+    const session = await mongoose.startSession();
 
-    export const menuService = {
-      async postMenuIntoDB(data: any, file: Express.Multer.File & { path?: string }) {
-        const session = await mongoose.startSession();
-      
-        try {
-          session.startTransaction();
-      
-          const menudata = JSON.parse(data);
-      
-          if (file && file.path) {
-            const imageName = `${Math.floor(100 + Math.random() * 900)}`;
-            const { secure_url } = await uploadImgToCloudinary(imageName, file.path) as {
-              secure_url: string;
-            };
-            menudata.image = secure_url;
-          } else {
-            menudata.image = 'no image';
-          }
-      
-        
-          const validatedData = await validateData<IMenu>(menuPostValidation, menudata);
-      
-          const restaurant = await RestaurantModel.findById(validatedData.restaurant).session(session);
-          if (!restaurant) {
-            throw new AppError(400, "Restaurant doesn't exist");
-          }
-      
-          const menu = await MenuModel.create([validatedData], { session });
-          const createdMenu = menu[0];
-      
-  
-          await RestaurantModel.findByIdAndUpdate(
-            validatedData.restaurant,
-            { $push: { menus: createdMenu._id } },
-            { session }
-          );
-      
-          await session.commitTransaction();
-          session.endSession();
-      
-          return createdMenu;
-      
-        } catch (error: unknown) {
-          // Rollback in case of any failure
-          await session.abortTransaction();
-          session.endSession();
-      
-          throw error;
-        }
-      },
-      async getAllMenuFromDB(query: any) {
-      try {
-      const result  = await  MenuModel.find({})
-          return {
-            result
-          };
-    
-         } catch (error: unknown) {
-          if (error instanceof Error) {
-            throw new Error(`${error.message}`);
-          } else {
-            throw new Error("An unknown error occurred while fetching by ID.");
-          }
-        }
-      },
-      async getSingleMenuFromDB(id: string) {
-        try {
-       const result =  await MenuModel.findOne({_id:id});
-       if(!result) {
-        throw new AppError(400,"Menu does not exist")
-       }
-       return result;
-       
-         } catch (error: unknown) {
-          if (error instanceof Error) {
-            throw new Error(`${error.message}`);
-          } else {
-            throw new Error("An unknown error occurred while fetching by ID.");
-          }
-        }
-      },
+    try {
+      session.startTransaction();
 
-   
-async getMenuWithRestaurantFromDB(id: string) {
-  try {
-    return await MenuModel.find({ restaurant: new Types.ObjectId(id) });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      throw new Error(`${error.message}`);
-    } else {
-      throw new Error("An unknown error occurred while fetching by ID.");
+      const menudata = JSON.parse(data);
+
+      if (file && file.path) {
+        const imageName = `${Math.floor(100 + Math.random() * 900)}`;
+        const { secure_url } = (await uploadImgToCloudinary(
+          imageName,
+          file.path
+        )) as {
+          secure_url: string;
+        };
+        menudata.image = secure_url;
+      } else {
+        menudata.image = null;
+      }
+
+      const validatedData = await validateData<IMenu>(
+        menuPostValidation,
+        menudata
+      );
+
+      const restaurant = await RestaurantModel.findById(
+        validatedData.restaurant
+      ).session(session);
+      if (!restaurant) {
+        throw new AppError(400, "Restaurant doesn't exist");
+      }
+
+      const menu = await MenuModel.create([validatedData], { session });
+      const createdMenu = menu[0];
+
+      await RestaurantModel.findByIdAndUpdate(
+        validatedData.restaurant,
+        { $push: { menus: createdMenu._id } },
+        { session }
+      );
+
+      await session.commitTransaction();
+      session.endSession();
+
+      return createdMenu;
+    } catch (error: unknown) {
+      // Rollback in case of any failure
+      await session.abortTransaction();
+      session.endSession();
+
+      throw error;
     }
-  }
-},
+  },
+  async getAllMenuFromDB(query: any) {
+    try {
+      const result = await MenuModel.find({});
+      return {
+        result,
+      };
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`${error.message}`);
+      } else {
+        throw new Error("An unknown error occurred while fetching by ID.");
+      }
+    }
+  },
+  async getSingleMenuFromDB(id: string) {
+    try {
+      const result = await MenuModel.findOne({ _id: id });
+      if (!result) {
+        throw new AppError(400, "Menu does not exist");
+      }
+      return result;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`${error.message}`);
+      } else {
+        throw new Error("An unknown error occurred while fetching by ID.");
+      }
+    }
+  },
 
-  async updateMenuIntoDB(data:IMenu,id: string) {
-    const validatedData = await validateData(menuUpdateValidation, data) as mongoose.UpdateQuery<IMenu>;
+  async getMenuWithRestaurantFromDB(id: string) {
+    try {
+      return await MenuModel.find({ restaurant: new Types.ObjectId(id) });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`${error.message}`);
+      } else {
+        throw new Error("An unknown error occurred while fetching by ID.");
+      }
+    }
+  },
 
-  
+  async updateMenuIntoDB(data: IMenu, id: string) {
+    const validatedData = (await validateData(
+      menuUpdateValidation,
+      data
+    )) as mongoose.UpdateQuery<IMenu>;
+
     const existingMenu = await MenuModel.findById(id);
     if (!existingMenu) {
       throw new AppError(status.NOT_FOUND, "Menu not found");
@@ -119,28 +124,25 @@ async getMenuWithRestaurantFromDB(id: string) {
     const updatedMenu = await MenuModel.findByIdAndUpdate(id, validatedData, {
       new: true,
     });
-  
+
     return updatedMenu;
-      },
-      async deleteMenuFromDB(id: string) {
-        try {
-    
- 
-        const isExist = await MenuModel.findOne({ _id: id });
-    
-        if (!isExist) {
-          throw new AppError(status.NOT_FOUND, "menu not found");
-        }
-    
-        await MenuModel.findByIdAndDelete({ _id: id });
-        return;
-    
-         } catch (error: unknown) {
-          if (error instanceof Error) {
-            throw new Error(`${error.message}`);
-          } else {
-            throw new Error("An unknown error occurred while fetching by ID.");
-          }
-        }
-      },
-    };
+  },
+  async deleteMenuFromDB(id: string) {
+    try {
+      const isExist = await MenuModel.findOne({ _id: id });
+
+      if (!isExist) {
+        throw new AppError(status.NOT_FOUND, "menu not found");
+      }
+
+      await MenuModel.findByIdAndDelete({ _id: id });
+      return;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`${error.message}`);
+      } else {
+        throw new Error("An unknown error occurred while fetching by ID.");
+      }
+    }
+  },
+};
