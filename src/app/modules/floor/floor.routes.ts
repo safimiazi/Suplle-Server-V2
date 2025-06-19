be7cda0a98df608679fdd58c6cb53fb200dbc8a5
abdul alim <abdulalim@bdcallings-Mac-mini-5.local>
2025-06-19 14:14:37 +0600

    update

--- src/app/modules/floor/floor.routes.ts
+++ src/app/modules/floor/floor.routes.ts
@@ -1,17 +1,17 @@
 
-    import express from "express";
-    import { validateRequest } from "../../middlewares/validateRequest";
-    import { floorController } from "./floor.controller";
-    import { floorPostValidation,floorUpdateValidation } from "./floor.validation";
+import express from "express";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { floorController } from "./floor.controller";
+import { floorPostValidation, floorUpdateValidation } from "./floor.validation";
 import { authenticate } from "../../middlewares/authGuard";
 import { ROLE } from "../users/user/users.constant";
 
-    const router = express.Router();
-    
-    router.post("/create-floor", authenticate(ROLE.RESTAURANT_OWNER), floorController.postFloor);
-    router.get("/all-floor",authenticate(ROLE.RESTAURANT_OWNER), floorController.getAllFloor);
-    router.get("/single-floor/:id",authenticate(ROLE.RESTAURANT_OWNER), floorController.getSingleFloor);
-    router.put("/update-floor/:id",authenticate(ROLE.RESTAURANT_OWNER),  floorController.updateFloor);
-    router.delete("/delete-floor/:id",authenticate(ROLE.RESTAURANT_OWNER), floorController.deleteFloor);
-    
-    export const floorRoutes = router;
+const router = express.Router();
+
+router.post("/create-floor", authenticate(ROLE.RESTAURANT_OWNER), floorController.postFloor);
+router.get("/all-floor", authenticate(ROLE.ADMIN, ROLE.STAFF, ROLE.DINE_IN, ROLE.RESTAURANT_OWNER, ROLE.TAKEAWAY), floorController.getAllFloor);
+router.get("/single-floor/:id", authenticate(ROLE.ADMIN, ROLE.STAFF, ROLE.DINE_IN, ROLE.RESTAURANT_OWNER, ROLE.TAKEAWAY), floorController.getSingleFloor);
+router.put("/update-floor/:id", authenticate(ROLE.RESTAURANT_OWNER), floorController.updateFloor);
+router.delete("/delete-floor/:id", authenticate(ROLE.RESTAURANT_OWNER), floorController.deleteFloor);
+
+export const floorRoutes = router;
